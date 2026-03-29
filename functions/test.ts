8269ae5272c1992965a0860e4a3e862991f5ef12
小林大樹 <blackfalljapan@gmail.com>
2026-03-29 16:12:45 +0900

    move functions into frontend

--- functions/test.ts
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  await context.env.POKEMON.put("test", "hello world")
-  const value = await context.env.POKEMON.get("test")
-  return new Response(value)
-}
